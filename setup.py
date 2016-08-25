16fc15c1d8d1aedd0ab64365c5798d1fcbec6ffc
therightman <therightmandev@gmail.com>
2016-08-25 12:35:33 +0100

    added files necessary for pypi

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+  name='slack-wrapper',
+  packages=['slack_wrapper'],
+  install_requires=[
+    'requests',
+    'websocket-client'
+  ],
+  version='0.1',
+  description='A wrapper for the slack API and RTM',
+  author='therightman',
+  author_email='therightmandev@gmail.com',
+  url='https://github.com/therightmandev/slack-wrapper',
+  license='MIT',
+  download_url='https://github.com/therightmandev/slack-wrapper/tarball/0.1',
+  keywords=['slack', 'api', 'bot', 'bots'],
+  classifiers=[],
+)
